4a87ef33a0b44c7f5da39be6b4e5214ad103b001
Tiago Oliveira <tfaoliveira@gmail.com>
2024-02-01 14:40:42 +0000

    mlkem - keccak testing for avx2 pack and unpack

--- jasmin/mlkem/sample_ntt/round3/test/keccakf1600.c
+++ jasmin/mlkem/sample_ntt/round3/test/keccakf1600.c
@@ -13,6 +13,10 @@
 #define KECCAKF1600_RANDOM_TESTS 100000
 #endif
 
+extern void keccakf1600(uint8_t state_ptr[25*8]);
+extern void pack(uint64_t state_ptr[28]);
+extern void unpack(uint64_t state_ptr[28]);
+
 void print_u64_5x5(const uint64_t *ptr)
 {
   size_t i;
@@ -34,7 +38,52 @@ void print_u64_5x5(const uint64_t *ptr)
   return;
 }
 
-extern void keccakf1600(uint8_t state_ptr[25*8]);
+void print_u64_5x5_p_3(const uint64_t *ptr)
+{
+  size_t i;
+
+  if(ptr == NULL) { return; }
+
+  printf("{\n  ");
+  for(i=0; i<24; i++)
+  { printf("0x%016" PRIx64 ", ", ptr[i]);
+
+    if((i+1)%5 == 0)
+    { printf("\n  "); }
+
+    if((i+1)%25 == 0)
+    { printf("\n  "); }
+  }
+
+  printf("0x%016" PRIx64 "\n",     ptr[i]);
+  printf("0x%016" PRIx64 ", ",     ptr[i+1]);
+  printf("0x%016" PRIx64 ", ",     ptr[i+2]);
+  printf("0x%016" PRIx64 "\n};\n", ptr[i+3]);
+  return;
+}
+
+void test_keccakf1600_avx2_pack_unpack()
+{
+  uint64_t *state_ptr;
+
+  state_ptr = malloc(sizeof(uint8_t) * (25+3) * 8);
+  assert(state_ptr != NULL);
+
+  for(uint64_t i=0; i<25; i++)
+  { state_ptr[i] = i+1; }
+  state_ptr[25] = -1;
+  state_ptr[26] = -1;
+  state_ptr[27] = -1;
+
+  pack(state_ptr);
+  print_u64_5x5_p_3((uint64_t*) state_ptr);
+
+  unpack(state_ptr);
+  print_u64_5x5_p_3((uint64_t*) state_ptr);
+
+  free(state_ptr);
+}
+
 
 void test_keccakf1600_00000()
 {
@@ -99,5 +148,6 @@ int main(void)
   //test_keccakf1600_00000();
   test_keccakf1600_01234();
   //test_keccakf1600_checksum();
+  //test_keccakf1600_avx2_pack_unpack();
   return 0;
 }
